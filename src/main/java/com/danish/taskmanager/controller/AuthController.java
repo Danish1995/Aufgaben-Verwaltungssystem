bce4480095ffd710d5988cbd687af58b6545a8cd
danish <d.m.javid95@gmail.com>
2026-04-02 11:37:22 +0200

    Refact: LoginDTO class added

--- src/main/java/com/danish/taskmanager/controller/AuthController.java
+++ src/main/java/com/danish/taskmanager/controller/AuthController.java
@@ -1,13 +1,16 @@
 package com.danish.taskmanager.controller;
 
 
+import com.danish.taskmanager.dto.LoginDTO;
 import com.danish.taskmanager.dto.UserRequestDTO;
 import com.danish.taskmanager.entity.User;
 import com.danish.taskmanager.repository.UserRepository;
 import com.danish.taskmanager.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Objects;
 
@@ -22,6 +25,7 @@ public class AuthController {
         this.userService = userService;
         this.userRepository = userRepository;
     }
+
     @GetMapping("/loginForm")
     public String addUser(Model model) {
         UserRequestDTO newUser = new UserRequestDTO();
@@ -31,9 +35,10 @@ public class AuthController {
 
     @PostMapping("/register")
     public String register(UserRequestDTO dto) {
-       userService.registerUser(dto);
+        userService.registerUser(dto);
         return "redirect:/users";
     }
+
     @GetMapping("/registerNewUserForm")
     public String registerNewUserForm(Model model) {
         UserRequestDTO newUser = new UserRequestDTO();
@@ -42,13 +47,8 @@ public class AuthController {
     }
 
     @GetMapping("/login")
-    public String login(UserRequestDTO dto) {
+    public String login(LoginDTO dto) {
         User byEmail = userRepository.findByEmail(dto.getEmail()); // call service layer here
-        System.out.println(byEmail.getEmail());
-        System.out.println(byEmail.getPassword());
-        System.out.println(dto.getEmail());
-
-        System.out.println(dto.getPassword());
         if (Objects.equals(byEmail.getPassword(), dto.getPassword())) {
             return "redirect:/users";// later → JWT token
         } else {
