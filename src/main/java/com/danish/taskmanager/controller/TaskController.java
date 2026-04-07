8dbc78e407bc42a9feeef0c70439866414386e20
danish <d.m.javid95@gmail.com>
2026-04-07 18:03:45 +0200

    Feat: get all task

--- src/main/java/com/danish/taskmanager/controller/TaskController.java
+++ src/main/java/com/danish/taskmanager/controller/TaskController.java
@@ -1,26 +1,41 @@
 package com.danish.taskmanager.controller;
 
+import com.danish.taskmanager.dto.TaskResponseDTO;
 import com.danish.taskmanager.entity.Task;
 import com.danish.taskmanager.entity.User;
 import com.danish.taskmanager.repository.TaskRepository;
 import com.danish.taskmanager.repository.UserRepository;
 
+import com.danish.taskmanager.service.TaskService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class TaskController {
 
     TaskRepository taskRepository;
+    TaskService taskService;
 
-    public TaskController(TaskRepository taskRepository) {
+    public TaskController(TaskRepository taskRepository, TaskService taskService) {
         this.taskRepository = taskRepository;
+        this.taskService = taskService;
     }
 
     @GetMapping("/tasks")
-    public List<Task> tasks() {
-        return taskRepository.findAll();
+    public String tasks(Model model) {
+        List<TaskResponseDTO> allTask = taskService.getAllTask();
+        model.addAttribute("tasks",allTask);
+        return "task/list-tasks";
+    }
+
+    @DeleteMapping("/deletetask")
+    public String deletetask(){
+
+        return "redirect/tasks";
     }
 }
