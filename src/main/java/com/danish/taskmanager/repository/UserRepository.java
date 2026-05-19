0281376f351c55978d2fb4cccf0820a17bf8b7ba
danish <d.m.javid95@gmail.com>
2026-05-19 16:55:49 +0200

    Test: TaskServiceTest tests are added

--- src/main/java/com/danish/taskmanager/repository/UserRepository.java
+++ src/main/java/com/danish/taskmanager/repository/UserRepository.java
@@ -10,4 +10,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
     Optional<User> findByEmail(String email);
     Optional<User> findById(Long id);
+
+//    findById() always returns Optional<User> → you can always use .orElseThrow() on it.
+//    Optional and orElseThrow are partners — orElseThrow only works on Optional, nothing else.
+//    int vs Long has absolutely nothing to do with Optional — it's a completely separate topic.
 }
